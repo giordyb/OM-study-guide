58aa728f4ae692018a1cf3575d4e8d239e69c5d7
Giordano Bianchi <giordy@giordy.it>
2020-11-09 23:03:14 +0100

    cambio font

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-pandoc -V geometry:a4paper -V geometry:margin=2cm questioni_di_esame.md -o questioni_di_esame.pdf 
-#pandoc -V geometry:left=1cm,right=1cm,top=2cm,bottom=2cm study_guide.md -o study_guide.pdf
-#pandoc -V geometry:left=1cm,right=1cm,top=2cm,bottom=2cm definizioni.md -o definizioni.pdf
-#pandoc -V geometry:a4paper -V geometry:margin=2cm tabelle.md -o tabelle.pdf
-#pandoc -V geometry:a4paper -V geometry:margin=2cm elettronica.md -o elettronica.pdf 
-#pandoc -V geometry:a4paper -V geometry:margin=2cm normativa.md -o normativa.pdf 
-#pandoc -V geometry:a4paper -V geometry:margin=2cm alfabeto.md -o alfabeto.pdf 
+pandoc -V geometry:a4paper -V 'mainfont:Helvetica' -V geometry:margin=2cm --pdf-engine=xelatex questioni_di_esame.md -o questioni_di_esame.pdf 
+pandoc -V geometry:a4paper -V 'mainfont:Helvetica' -V geometry:margin=2cm --pdf-engine=xelatex study_guide.md -o study_guide.pdf
+pandoc -V geometry:a4paper -V 'mainfont:Helvetica' -V geometry:margin=2cm --pdf-engine=xelatex definizioni.md -o definizioni.pdf
+pandoc -V geometry:a4paper -V 'mainfont:Helvetica' -V geometry:margin=2cm --pdf-engine=xelatex tabelle.md -o tabelle.pdf
+pandoc -V geometry:a4paper -V 'mainfont:Helvetica' -V geometry:margin=2cm --pdf-engine=xelatex elettronica.md -o elettronica.pdf 
+pandoc -V geometry:a4paper -V 'mainfont:Helvetica' -V geometry:margin=2cm --pdf-engine=xelatex normativa.md -o normativa.pdf 
+pandoc -V geometry:a4paper -V 'mainfont:Helvetica' -V geometry:margin=2cm --pdf-engine=xelatex alfabeto.md -o alfabeto.pdf 
